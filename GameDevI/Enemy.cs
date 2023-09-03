57c1df9c5c11a678a0dd3f6199fbcd46d4852671
Deltafive-Piya <pierstoddard@gmail.com>
2023-09-03 02:00:07 -0400

    complete-GDI

---
+++ GameDevI/Enemy.cs
@@ -0,0 +1,46 @@
+// public Enemy #1
+public class Enemy
+{
+    // Every Enemy will consist of stats(.this):
+    private string Name;
+    public string _Name {get {return Name;}} //implementation post-error CS0122: 'Enemy.Name' is inaccessible due to its protection level [C:\C#\GameDevI\GameDevI.csproj]
+    private int health;
+    private List<Attack> AttackList;
+
+    // public instance
+    public Enemy(string name)
+    {
+        Name = name;
+        health = 100;
+        AttackList = new List<Attack>();
+    }
+
+    // Public instance of private.health
+    public int Health
+    {
+        get { return health; }
+    }
+
+    // Method- Add +1 Fight knowledge
+    public void AddAttack(Attack attack)
+    {
+        AttackList.Add(attack);
+    }
+
+    // Method- Random Attack
+    public void RandomAttack()
+    {
+        if (AttackList.Count == 0)
+        {
+            Console.WriteLine($"{_Name} has no attacks available.");
+            return;
+        }
+
+        Random random = new Random();
+        int randomIndex = random.Next(AttackList.Count);
+        Attack selectedAttack = AttackList[randomIndex];
+
+        Console.WriteLine($"{_Name} performs {selectedAttack.Name} for {selectedAttack.DamageAmount} damage!");
+    }
+
+}
---
+++ GameDevI/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Create an instance of an Enemy
+        Enemy enemy = new Enemy("Bandit");
+
+        // Create 3 instances of Attacks
+        Attack fireball = new Attack("Fireball", 20);
+        Attack punch = new Attack("Punch", 10);
+        Attack throwAttack = new Attack("Throw", 15);
+
+        // Add these Attacks to the Enemy's Attack List
+        enemy.AddAttack(fireball);
+        enemy.AddAttack(punch);
+        enemy.AddAttack(throwAttack);
+
+        // Test RandomAttack method
+        Console.WriteLine($"{enemy._Name}'s Health: {enemy.Health}");
+        enemy.RandomAttack();
+        enemy.RandomAttack();
+        enemy.RandomAttack();
+        Console.WriteLine($"{enemy._Name}'s Health: {enemy.Health}");
+    }
+}

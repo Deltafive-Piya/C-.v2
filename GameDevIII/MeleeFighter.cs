1b5173761a7189197ecbec09532db2e4ccb58b3f
Deltafive-Piya <pierstoddard@gmail.com>
2023-09-03 17:36:06 -0400

    create- GameDevIII

---
+++ GameDevIII/MeleeFighter.cs
@@ -0,0 +1,21 @@
+public class MeleeFighter : Enemy
+{
+    public MeleeFighter(string name) : base(name)
+    {
+        // Set health and add melee fighter attacks
+        health = 120;
+        AddAttack(new Attack("Spinning Elbow", 20));
+        AddAttack(new Attack("Flying Backkick", 15));
+        AddAttack(new Attack("Superman Punch", 25));
+    }
+
+    public void Rage(Enemy target)
+    {
+        // Action- New Random (attack +10DA)
+        Random random = new Random();
+        int randomIndex = random.Next(AttackList.Count);
+        Attack chosenAttack = AttackList[randomIndex];
+        chosenAttack.DamageAmount += 10;
+        PerformAttack(target, chosenAttack);
+    }
+}

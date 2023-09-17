b0cccbde4a6dbd17031ce5fb2ad194804f5619ef
Deltafive-Piya <pierstoddard@gmail.com>
2023-09-16 23:25:22 -0400

    FirstTest-DojoSurveyWithModel

--- DojoSurveyWithModel/Controllers/HomeController.cs
+++ DojoSurveyWithModel/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿//ln 18
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using DojoSurveyWithModel.Models;
 
@@ -6,6 +7,8 @@ namespace DojoSurveyWithModel.Controllers;
 
 public class HomeController : Controller
 {
+    static User NewUser; //1. make this so we can pass it in the form via action="process"
+
     private readonly ILogger<HomeController> _logger;
 
     public HomeController(ILogger<HomeController> logger)
@@ -13,14 +16,25 @@ public class HomeController : Controller
         _logger = logger;
     }
 
+    // Start getting and postin here
+    [HttpGet("")]
     public IActionResult Index()
     {
         return View();
     }
 
-    public IActionResult Privacy()
+    [HttpPost("process")]
+    public IActionResult Process(User user) //2. pass the user here
     {
+        NewUser = user; //3. populate new user with user here
         return View();
+    }
+
+    [HttpGet("result")]
+    public IActionResult Result()
+    {
+        return View(NewUser); //4. Return the NewUser ; 
+                                //Now our result is going to recieve a view model...
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ DojoSurveyWithModel/build.md
@@ -0,0 +1,6 @@
+dotnet new mvc --no-https -o DojoSurveyWithModel
+cd DojoSurveyWithModel
+code .
+
+1) Create Model
+    

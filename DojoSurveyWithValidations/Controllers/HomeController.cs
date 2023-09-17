6d64d3612eb2265e65139abeade1f0f19e726bb9
Deltafive-Piya <pierstoddard@gmail.com>
2023-09-16 23:58:47 -0400

    MigrateDJWM-DojoSurveyWithValidations

--- DojoSurveyWithValidations/Controllers/HomeController.cs
+++ DojoSurveyWithValidations/Controllers/HomeController.cs
@@ -1,26 +1,38 @@
-﻿using System.Diagnostics;
+﻿//ln 18
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using DojoSurveyWithValidations.Models;
-
 namespace DojoSurveyWithValidations.Controllers;
 
 public class HomeController : Controller
 {
+    static User NewUser; //1. make this so we can pass it in the form via action="process"
+
     private readonly ILogger<HomeController> _logger;
 
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
     }
-
+    // Start getting and postin here
+    [HttpGet("")]
     public IActionResult Index()
     {
         return View();
     }
 
-    public IActionResult Privacy()
+    [HttpPost("process")]
+    public IActionResult Process(User user) //2. pass the user here
     {
-        return View();
+        NewUser = user; //3. populate new user with user here
+        return RedirectToAction("Result");
+    }
+
+    [HttpGet("result")]
+    public IActionResult Result()
+    {
+        return View(NewUser); //4. Return the NewUser ; 
+                                //Now our result is going to recieve a view model...
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ DojoSurveyWithValidations/build.md
@@ -0,0 +1,16 @@
+dotnet new mvc --no-https -o DojoSurveyWithValidations
+cd DojoSurveyWithValidations
+code .
+
+1) Migrate:
+    1. Model-User.cs
+    2. Index.cshtml
+    3. site.css
+    4. HomeController
+    5. Result.cshtml
+    
+2) 
+
+3) 
+
+4) 
